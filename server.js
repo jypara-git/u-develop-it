d69288a573c893243c27ea620b7285416c601237
jypara-git <zhyparaabdieva@gmail.com>
2021-04-19 13:01:16 -0700

    build the database calls

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your mySQL username,
+        user: 'root',
+        // your mySQL password
+        password: 'Password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
