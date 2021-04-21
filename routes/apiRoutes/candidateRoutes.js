399f5ffe67040498b6ebded72527a38a2def464d
jypara-git <zhyparaabdieva@gmail.com>
2021-04-21 00:06:24 -0700

    added voter routes

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -90,7 +90,7 @@ router.post('/candidate', ({ body}, res) => {
 });
 
 // Update a candidate's party
-app.put('/api/candidate/:id', (req, res) => {
+router.put('/candidate/:id', (req, res) => {
     const errors = inputCheck(req.body, 'party_id');
 
     if (errors) {
